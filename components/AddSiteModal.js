130662fd0c5678a2ec5f676be4eed105b3632ba4
Bastien DUMONT <bastienxs@gmail.com>
2022-12-11 13:14:49 +0100

    fix: id site when creating

--- components/AddSiteModal.js
+++ components/AddSiteModal.js
@@ -36,7 +36,7 @@ const AddSiteModal = ({ children }) => {
       url,
     };
 
-    createSite(newSite);
+    const { id } = createSite(newSite);
     toast({
       title: 'Success!',
       description: "We've added your site.",
@@ -46,9 +46,9 @@ const AddSiteModal = ({ children }) => {
     });
     mutate(
       ['/api/sites', auth.user.token],
-      async (data) => {
-        return { sites: [...data.sites, newSite] };
-      },
+      async (data) => ({
+        sites: [...data.sites, { id, ...newSite }],
+      }),
       false
     );
     onClose();
