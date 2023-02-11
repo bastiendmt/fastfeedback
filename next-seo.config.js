48fa3366b0412b889f122930dcf17d76424f1b42
Bastien Dumont <32489032+bastiendmt@users.noreply.github.com>
2023-02-11 15:52:29 +0100

    Seo (#3)
    * fix import
    
    * use env vars
    
    * firebase keys
    
    * remove import
    
    * fix feedback page
    
    * pino
    
    * fixes
    
    * next seo
    
    * favicons
    
    * add seo config
    
    * page title
    
    * page component

---
+++ next-seo.config.js
@@ -0,0 +1,26 @@
+const title =
+  'Fast Feedback – The easiest way to add comments or reviews to your static site.';
+const description = 'Fast Feedback is being built as part of React 2025.';
+
+const SEO = {
+  title,
+  description,
+  canonical: 'fastfeedback-three-nu.vercel.app',
+  openGraph: {
+    type: 'website',
+    locale: 'en_IE',
+    url: 'fastfeedback-three-nu.vercel.app',
+    title,
+    description,
+    images: [
+      {
+        url: 'fastfeedback-three-nu.vercel.app/op.png',
+        alt: title,
+        width: 1280,
+        height: 720,
+      },
+    ],
+  },
+};
+
+export default SEO;
