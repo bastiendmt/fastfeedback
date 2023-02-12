e4a4625d47eda3275ce331e0ae805ad2a22e59a5
Bastien DUMONT <bastienxs@gmail.com>
2023-02-12 11:18:45 +0100

    embed

---
+++ pages/embed/[siteId].js
@@ -0,0 +1,84 @@
+import { Feedback } from '@/components/Feedback';
+import { useAuth } from '@/lib/auth';
+import { createFeedback } from '@/lib/db';
+import { getAllFeedback, getAllSites } from '@/lib/db-admin';
+import { Box, Button, FormControl, FormLabel, Input } from '@chakra-ui/react';
+import { useRouter } from 'next/dist/client/router';
+import { useRef, useState } from 'react';
+
+const SiteFeedback = ({ initialFeedback }) => {
+  const auth = useAuth();
+  const router = useRouter();
+  const inputEl = useRef(null);
+  const [allFeedback, setAllFeedback] = useState(initialFeedback);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const text = e.target.elements.comment;
+
+    const newFeedback = {
+      author: auth.user.name,
+      authorId: auth.user.uid,
+      siteId: router.query.siteId,
+      text,
+      createdAt: new Date().toISOString(),
+      provider: auth.user.provider,
+      status: 'pending',
+      rating: 5,
+    };
+    inputEl.current.value = '';
+    setAllFeedback([newFeedback, ...allFeedback]);
+    createFeedback(newFeedback);
+  };
+
+  return (
+    <Box display="flex" flexDirection="column" width="full">
+      <Box as="form" onSubmit={onSubmit}>
+        <FormControl my={8}>
+          <FormLabel htmlFor="comment">Comment</FormLabel>
+          <Input ref={inputEl} type="comment" id="comment" />
+          <Button
+            mt={2}
+            type="submit"
+            fontWeight="medium"
+            disabled={router.isFallback}
+          >
+            Add Comment
+          </Button>
+        </FormControl>
+      </Box>
+      {allFeedback &&
+        allFeedback.map((feedback) => (
+          <Feedback key={feedback.id} {...feedback} />
+        ))}
+    </Box>
+  );
+};
+
+export async function getStaticProps(context) {
+  const siteId = context.params.sideId;
+  const { feedback } = await getAllFeedback(siteId);
+
+  return {
+    props: {
+      initialFeedback: feedback,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const { sites } = await getAllSites();
+
+  const paths = sites.map((site) => ({
+    params: {
+      siteId: site.id.toString(),
+    },
+  }));
+
+  return {
+    paths,
+    fallback: true,
+  };
+}
+
+export default SiteFeedback;
